b440aeee615a0f86be33083c01496de36fe32e1d
TAEHEE <jangth0655@gmail.com>
2025-04-13 00:19:06 +0900

    공통 컴포넌트 버튼 스타일링 업데이트

---
+++ src/components/Button.tsx
@@ -0,0 +1,87 @@
+'use client';
+
+import { ButtonHTMLAttributes } from 'react';
+import clsx from 'clsx';
+
+type Variants = 'primary' | 'danger' | 'secondary' | 'text';
+type Tone = 'green' | 'red' | 'gray';
+type Size = 'lg' | 'md' | 'sm';
+
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
+  text: string;
+  variants: Variants;
+  size: Size;
+  loading?: boolean;
+  tone: Tone;
+}
+
+export default function Button({ text, size, loading, variants, tone, ...rest }: Props) {
+  const styleMap: Record<Variants, Record<Tone, string>> = {
+    primary: {
+      green:
+        'bg-primary-G400 text-white hover:bg-primary-300 active:bg-primary-500 disabled:bg-secondary-N40 disabled:text-white border-0',
+      gray: '',
+      red: '',
+    },
+    danger: {
+      red: 'border-0 text-white bg-system-r-R400 hover:bg-system-r-R300 active:bg-system-r-R500 disabled:bg-secondary-N40',
+      green: '',
+      gray: '',
+    },
+    secondary: {
+      green:
+        'border-[1px] bg-transparent text-primary-400 border-primary-400  hover:text-primary-300 hover:bg-primary-300 hover:border-primary-300 active:text-primary-500 active:border-primary-500 active:bg-secondary-N30 disabled:border-secondary-N40 disabled:text-secondary-N40',
+      gray: 'bg-transparent border border-secondary-N40 text-black hover:bg-secondary-N10 hover:border-secondary-N60 active:bg-secondary-N30 active:border-secondary-N90 disabled:border-secondary-N40 disabled:text-secondary-N40',
+      red: 'bg-transparent border border-system-r-R400 text-system-r-R400  hover:border-system-r-R300 hover:text-system-r-R300 active:bg-secondary-N30 active:border-system-r-R500 active:text-system-r-R500 disabled:border-secondary-N40 disabled:text-secondary-N40',
+    },
+    text: {
+      green:
+        'bg-transparent border-0 text-primary-G400 hover:bg-secondary-N10 hover:text-primary-G400 active:bg-secondary-N30 active:text-primary-G500 disabled:text-secondary-N40',
+      gray: 'bg-transparent border-0 text-black hover:bg-secondary-N10 active-bg-secondary-N-30 disabled:text-secondary-N40',
+      red: 'bg-transparent border-0 text-system-r-R400 hover:bg-secondary-N10 hover:text-system-r-R300 active:bg-secondary-N30 active:text-system-r-R500 disabled:text-secondary-N40',
+    },
+  };
+
+  const loadingMap: Record<Variants, Record<Tone, string>> = {
+    primary: {
+      green: 'bg-primary-G400 text-white',
+      red: '',
+      gray: '',
+    },
+    danger: {
+      red: 'bg-system-r-R400 text-white',
+      green: '',
+      gray: '',
+    },
+    secondary: {
+      green: 'border-primary-400 text-primary-400',
+      red: 'border-system-r-R400 text-system-r-R400',
+      gray: 'border-secondary-N40 text-secondary-N40',
+    },
+    text: {
+      green: 'text-primary-G400',
+      red: 'text-system-r-R400',
+      gray: 'text-secondary-N40',
+    },
+  };
+
+  const sizeMap: Record<Size, string> = {
+    lg: 'py-[17px] label-lg',
+    md: 'py-[14px] label-md',
+    sm: 'py-[11.5px] label-sm',
+  };
+
+  return (
+    <button
+      disabled={loading || rest.disabled}
+      className={clsx(
+        loading ? loadingMap[variants][tone] : styleMap[variants][tone],
+        sizeMap[size],
+        'h-[56px] round-xs'
+      )}
+      {...rest}
+    >
+      {text}
+    </button>
+  );
+}
