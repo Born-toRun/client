0a685d613a5e0ed6b70b5431fd7e8e896fbf030a
taehee.gn <taehee.gn@taeheegnui-MacBookPro.local>
2025-05-17 16:18:09 +0900

    Dialog, Sheet UI & Login Bottom Sheet UI

---
+++ src/components/LoginBottomSheet.tsx
@@ -0,0 +1,61 @@
+'use client';
+
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+} from './Sheet';
+import DialogImageIcon from '@/icons/dialog-img.svg';
+import CloseIcon from '@/icons/close-icon.svg';
+import KakaoIcon from '@/icons/kakao-icon.svg';
+
+interface Props {
+  open: boolean;
+  onOpenChange: () => void;
+}
+
+export default function LoginBottomSheet({ onOpenChange, open }: Props) {
+  return (
+    <Sheet open={open} onOpenChange={onOpenChange}>
+      <SheetContent
+        className='w-full h-[640px] p-[16px] pb-[164px] bg-white rounded-t-[8px]'
+        side='bottom'
+      >
+        <SheetHeader className='flex w-full items-center h-[32px] justify-end'>
+          <button onClick={onOpenChange}>
+            <CloseIcon />
+          </button>
+        </SheetHeader>
+
+        <div>
+          <div className='my-[16px] w-full flex justify-center items-center'>
+            <DialogImageIcon />
+          </div>
+
+          <div className='flex flex-col gap-[8px] py-[16px] w-full'>
+            <SheetTitle className='headline-md text-center'>
+              본투런과 함께 뛰어요!
+            </SheetTitle>
+            <SheetDescription className='text-secondary-200 text-center'>
+              반가워요! 본투런 회원이 되어 간편한 러닝 모임 관리와 러너들과의
+              소통을 경험해보세요!
+            </SheetDescription>
+          </div>
+        </div>
+
+        <SheetFooter>
+          <button
+            type='button'
+            className='text-black bg-[#FEE500] rounded-[12px] w-full flex justify-center items-center h-[56px] gap-[8px] mt-[16px]'
+          >
+            <KakaoIcon />
+            카카오로 간편하게 시작하기
+          </button>
+        </SheetFooter>
+      </SheetContent>
+    </Sheet>
+  );
+}
--- src/features/feeds/list/index.tsx
+++ src/features/feeds/list/index.tsx
@@ -1,18 +1,19 @@
-"use client";
+'use client';
 
-import { useState } from "react";
-import { useGetFeesListQuery } from "./hooks/queries";
-import { useIntersectionObserver } from "@/features/hooks/useIntersectionObserver";
-import { useScrollPosition } from "@/features/hooks/useScroll";
+import { useState } from 'react';
+import { useGetFeesListQuery } from './hooks/queries';
+import { useIntersectionObserver } from '@/features/hooks/useIntersectionObserver';
+import { useScrollPosition } from '@/features/hooks/useScroll';
 
-import MainHeader from "@/components/header/MainHeader";
-import Tabs from "@/components/Tabs";
-import FeedList from "./components/FeedList";
-import CheckBox from "@/components/CheckBox";
-import CreateFeedButton from "./components/CreateFeedButton";
+import MainHeader from '@/components/header/MainHeader';
+import Tabs from '@/components/Tabs';
+import FeedList from './components/FeedList';
+import CheckBox from '@/components/CheckBox';
+import CreateFeedButton from './components/CreateFeedButton';
 
-import { FEEDCategory } from "./types";
-import { feedCategoryLabel } from "../constants";
+import { FEEDCategory } from './types';
+import { feedCategoryLabel } from '../constants';
+import LoginBottomSheet from '@/components/LoginBottomSheet';
 
 export default function FeedContainer() {
   const [selectedTabs, setSelectedTabs] = useState<FEEDCategory>(
@@ -39,16 +40,16 @@ export default function FeedContainer() {
   return (
     <div>
       <MainHeader selectedTabs={selectedTabs} isScrolled={isScrolled} />
-      <div className="pt-[68px] mb-[16px]">
+      <div className='pt-[68px] mb-[16px]'>
         <Tabs
           options={feedListTabOptions}
           selectedTabs={selectedTabs}
           onSelectedTab={setSelectedTabs}
         />
       </div>
-      <div className="px-[16px] flex items-center h-[40px]">
+      <div className='px-[16px] flex items-center h-[40px]'>
         <CheckBox
-          text="크루 공개 글 보기"
+          text='크루 공개 글 보기'
           onChange={() => setIsMyCrew((prev) => !prev)}
           checked={isMyCrew}
         />
@@ -56,6 +57,18 @@ export default function FeedContainer() {
 
       <FeedList list={feedList} />
       {feedList && feedList.length > 0 && <div ref={setTargetRef} />}
+
+      {/* <CustomDialog
+        open
+        onOpenChange={() => {}}
+        contents={{
+          title: '로그인이 필요해요',
+          description:
+            '러네이서 회원이 되면 러닝 모임 관리와 소통이 훨씬 간편해져요!',
+        }}
+      /> */}
+
+      <LoginBottomSheet onOpenChange={() => {}} open />
 
       <CreateFeedButton isScrolled={isScrolled} />
     </div>
@@ -65,10 +78,10 @@ export default function FeedContainer() {
 const feedListTabOptions: { key: FEEDCategory; label: string }[] = [
   {
     key: feedCategoryLabel.COMMUNITY,
-    label: "커뮤니티",
+    label: '커뮤니티',
   },
   {
     key: feedCategoryLabel.MARKET,
-    label: "마켓",
+    label: '마켓',
   },
 ];
