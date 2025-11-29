114560d2dc3c2eebbda39791eb498637b98349e9
small-goliath <meang617@gmail.com>
2025-11-29 12:02:55 +0900

    feat(next.config.ts): 환경 변수 BACKEND_URL을 사용하여 API 경로 설정 가능하도록 수정

--- next.config.ts
+++ next.config.ts
@@ -39,7 +39,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/v1/:path*",
-        destination: "https://b2r.kro.kr/api/v1/:path*",
+        destination: `${process.env.BACKEND_URL || "https://be.b2r.kro.kr"}/api/v1/:path*`
       },
     ];
   },
