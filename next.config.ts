8807afb83fcc03c6e55964537b565bc4f7385477
small-goliath <meang617@gmail.com>
2025-11-29 12:39:18 +0900

    feat(next.config.ts): OAuth2 콜백 경로 리다이렉션 추가

--- next.config.ts
+++ next.config.ts
@@ -41,6 +41,10 @@ const nextConfig: NextConfig = {
         source: "/api/v1/:path*",
         destination: `${process.env.BACKEND_URL || "https://be.b2r.kro.kr"}/api/v1/:path*`
       },
+      {
+        source: "/login/oauth2/code/:provider",
+        destination: `${process.env.BACKEND_URL || "https://be.b2r.kro.kr"}/login/oauth2/code/:provider`,
+      }
     ];
   },
 };
